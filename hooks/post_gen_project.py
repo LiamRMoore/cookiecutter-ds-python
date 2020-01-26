efaae81177d847742ccb6d558dd9c8775d8d1541
Liam R Moore <lmoore90@gmail.com>
2020-01-26 17:27:35 +0100

    debug hooks again

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,12 +15,12 @@ create_aws_scripts = '{{cookiecutter.use_aws_ec2_instance}}' == 'y'
 
 if not create_aws_scripts:
     # remove relative file nested inside the generated folder
-    remove(os.path.join('{{cookiecutter.package_name}}', 'bin', 'connect_to_aws'))
-    remove(os.path.join('{{cookiecutter.package_name}}', 'bin', 'connect_to_notebook'))
-    remove(os.path.join('{{cookiecutter.package_name}}', 'bin', 'update_ssh_config'))
+    remove(os.path.join('{{cookiecutter.repo_name}}', 'bin', 'connect_to_aws'))
+    remove(os.path.join('{{cookiecutter.repo_name}}', 'bin', 'connect_to_notebook'))
+    remove(os.path.join('{{cookiecutter.repo_name}}', 'bin', 'update_ssh_config'))
 else:
     # sanity check
     if not os.path.isfile({{ cookiecutter.aws_ec2_ssh_key }}):
         warnings.warn("SSH key not found at {{ cookiecutter.aws_ec2_ssh_key }}!")
     # remove absolute path to file nested inside the generated folder
-    #remove(os.path.join(os.getcwd(), '{{cookiecutter.package_name}}', 'file_one.py'))
+    #remove(os.path.join(os.getcwd(), '{{cookiecutter.repo_name}}', 'file_one.py'))
